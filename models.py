77205d6894e691744f78bf67410ab4e5741f5c07
danpetitto <danpetitto@seznam.cz>
2024-09-12 09:19:37 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,23 +1,32 @@
-flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
+# Inicializace SQLAlchemy
 db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
+# Model pro uživatele
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    portfolios = relationship('Portfolio', back_populates='user')
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password) # type: ignore
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password) # type: ignore
+        return check_password_hash(self.password_hash, password)
 
+# Model pro portfolio
 class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     filename = db.Column(db.String(150), nullable=False)
     data = db.Column(db.LargeBinary, nullable=False)
+    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    owner = db.relationship('User', backref=db.backref('portfolios', lazy=True))
+    user = relationship('User', back_populates='portfolios')
