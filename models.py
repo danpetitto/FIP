81f4422f0b21204d4469f4df8430605ae87ddab7
danpetitto <danpetitto@seznam.cz>
2024-10-03 19:03:21 +0200

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
 # Inicializace SQLAlchemy
 db = SQLAlchemy()
@@ -30,3 +31,17 @@ class Portfolio(db.Model):
     
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = relationship('User', back_populates='portfolios')
+
+# Model pro obchod (Trade)
+class Trade(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'), nullable=False)
+    datum = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    typ_obchodu = db.Column(db.String(10), nullable=False)  # nákup/prodej
+    ticker = db.Column(db.String(10), nullable=False)
+    cena = db.Column(db.Float, nullable=False)
+    pocet = db.Column(db.Integer, nullable=False)  # nový sloupec pro počet
+    hodnota = db.Column(db.Float, nullable=False)
+    poplatky = db.Column(db.Float, nullable=False, default=0.0)
+
+
